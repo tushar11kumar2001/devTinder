18ac37507abe818a7cc40b9aab0b0f18064a20c8
tushar11kumar2001 <tushar11kumar2001@gmail.com>
2024-12-06 05:37:49 +0530

    create a server

--- src/app.js
+++ src/app.js
@@ -3,9 +3,13 @@ const express = require('express');
 const app = express();
 
 app.use("/",(req,res)=>{
+    console.log("home request");
+
     res.send("Welcome to Dashboard..");
 })
 app.use("/hello",(req,res)=>{
+    console.log("hello request");
+    
     res.send("Hello hello hello...");
 })
 
