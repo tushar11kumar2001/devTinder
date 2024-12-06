d86172518d2b58e096acd8eeae0861b03b562423
tushar11kumar2001 <tushar11kumar2001@gmail.com>
2024-12-06 06:58:27 +0530

    test get,post,delete api.

--- src/app.js
+++ src/app.js
@@ -2,12 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.use("/hello/friend",(req,res)=>{
-    res.send("can we friend?");
+app.get("/user",(req,res)=>{
+    res.send({
+        first_name:"Tushar",
+        last_name:"Kumar"
+    })
 })
-app.use("/hello",(req,res)=>{    
-    res.send("Hello hello hello...");
+app.post("/user",(req,res)=>{
+    console.log("USER data add...");
+    res.send("user added successfully..")
 })
+app.delete("/user",(req,res)=>{
+    res.send("user delete successfully...")
+})
+
+//this will match all http api method requests
 app.use("/",(req,res)=>{
     res.send("Welcome to Dashboard..")
 })
