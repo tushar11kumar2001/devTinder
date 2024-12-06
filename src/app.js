f8a68e5360de1ea4e5da12e89121daca8809f0ca
tushar11kumar2001 <tushar11kumar2001@gmail.com>
2024-12-07 05:00:02 +0530

    one route has multiple route handlers (next())

--- src/app.js
+++ src/app.js
@@ -1,17 +1,20 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
 
-//when you put ? after any letter it mean that letter is optional 
-//when you put + after any letter it mean that letter comes many times
-//when you put * it means anything can comes in between that pattern 
-app.get("/user",(req,res)=>{
-    console.log(req.query);
-    
-    res.send("successfull....")
-})
+app.use(
+  "/user",
+  (req, res, next) => {
+    console.log("1st route handle...");
+    next();
+    res.send("1st route handle");
+  },
+  (req, res) => {
+    console.log("2st route handle...");
+    res.send("2st route handle");
+  }
+);
 
-app.listen(1100,()=>{
-    console.log("Server is successfully listening on port 1100");
-    
+app.listen(1100, () => {
+  console.log("Server is successfully listening on port 1100");
 });
