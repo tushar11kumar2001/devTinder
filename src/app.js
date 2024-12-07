c52b28b3b9482854e7f00d0bc5e2985dc06268bb
tushar11kumar2001 <tushar11kumar2001@gmail.com>
2024-12-07 21:25:15 +0530

    build feed api

--- src/app.js
+++ src/app.js
@@ -1,8 +1,53 @@
 const connectDB = require("./config/database");
 const express = require("express");
-
+const UserModel = require("./models/user");
 const app = express();
 
+app.use(express.json())
+
+app.post("/signup",async(req,res)=>{
+    //creating a new instance of the UserModel
+    const user = new UserModel(req.body)
+    try{   
+        await user.save();
+        res.send("User added successfully..")
+    }catch(err){
+        throw new Error(err.message);
+    }
+  
+});
+//find the user by emailId...
+app.get("/user",async(req,res)=>{
+    const userEmail = req.body.emailId;
+    try{
+       const users = await UserModel.find({emailId:userEmail});
+       if(users.length === 0){
+        res.status(404).send("User not found");
+       }else{
+       res.send(users);
+       }
+    }catch(err){
+      res.send("Something went wrong");
+    }
+});
+//find all users for feed
+app.get("/feed",async(req,res)=>{
+    try{
+    const users = await UserModel.find({}).exec();
+    if(users.length === 0){
+        res.status(404).send("User not found");
+       }else{
+       res.send(users);
+       }
+    }catch(err){
+        res.send("Something went wrong");
+      }
+})
+app.use("/",(err,req,res,next)=>{
+    if(err){
+        res.status(400).send("something went wrong..");
+    }
+})
 connectDB()
     .then(()=>{
         console.log("Database connection established...");
