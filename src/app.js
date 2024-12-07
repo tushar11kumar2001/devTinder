1dc706108f046a0b31048b1530406914cf380c57
tushar11kumar2001 <tushar11kumar2001@gmail.com>
2024-12-07 05:42:45 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -2,20 +2,21 @@ const express = require("express");
 
 const app = express();
 
-app.use(
-  "/user",
-  (req, res, next) => {
-    console.log("1st route handle...");
-    next();
-  }
-);
-app.use(
-    "/user",
-    (req, res, next) => {
-      console.log("2nd route handle...");
-      res.send("2nd route handle...")
-    }
-  );
+// this is the authorization middleware..
+app.use("/admin",(req,res,next)=>{
+    const token = "xyz";
+    if(token === "xyz") next();
+    else res.send("You have not authorization..")
+    
+})
+app.get("/admin/getAllData",(req,res)=>{
+    res.send("Get all data...");
+})
+app.get("/admin/deleteUser",(req,res)=>{
+    res.send("Deleted user");
+})
+
+
 app.listen(1100, () => {
   console.log("Server is successfully listening on port 1100");
 });
