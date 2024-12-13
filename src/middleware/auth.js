d213ae840f828f66ea26a32c1f9eb73358c86bff
tushar11kumar2001 <tushar11kumar2001@gmail.com>
2024-12-14 03:41:08 +0530

    auth middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,10 +1,25 @@
-const adminAuth = (req,res,next)=>{
-    const token = "xy";
-    if(token === "xyz") next();
-    else res.send("You have not authorization..")
+const jwt = require("jsonwebtoken");
+const UserModel = require("../models/userSchema")
+const userAuth = async(req,res,next)=>{
+    try{
+    const { token } = req.cookies;
+    if(!token){
+    throw new Error("Invalid Token");
+    }
+    const decodeObj = await jwt.verify(token,"tusharkumar@123");
+    const { _id } = decodeObj;
+    const user = await UserModel.findById(_id);
     
+    if(!user){
+    throw new Error("User not found");
+    }
+    req.user = user;
+    next();
+    }catch(err){
+        res.send("ERROR : "+err.message);
+    }
 }
 
 module.exports = {
-    adminAuth
+    userAuth
 }
