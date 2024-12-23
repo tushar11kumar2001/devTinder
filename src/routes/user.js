b73d47c6583ecc63ca8c17dfc7d55864d69fad78
tushar11kumar2001 <tushar11kumar2001@gmail.com>
2024-12-23 23:11:16 +0530

    build feed api

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middleware/auth");
 const ConnectionRequestModel = require("../models/connectionRequestSchema");
+const UserModel = require("../models/userSchema");
 
 const userRouter = express.Router();
 
@@ -49,6 +50,43 @@ userRouter.get("/user/connection", userAuth, async (req, res) => {
     res.json({
       data: data,
     });
+  } catch (err) {
+    res.json({
+      message: `${err.message}`,
+      status: "400",
+    });
+  }
+});
+
+userRouter.get("/user/feed", userAuth, async (req, res) => {
+  //show all user in user collection expect :
+  // 1. which is present in connection request because if once any request has been sent then that user will not shown in feed
+  // 2. LoggedInUser itself
+  try {
+    const page = req.query.page;
+    const limit = req.query.limit;
+    const skip = (page*limit)-limit;
+    const loggedInUser = req.user;
+    const connectionRequest = await ConnectionRequestModel.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    }).select("fromUserId toUserId");
+    const hideUserFromFeed = new Set();
+    connectionRequest.forEach((e) => {
+      hideUserFromFeed.add(e.fromUserId.toString());
+      hideUserFromFeed.add(e.toUserId.toString());
+    });
+
+
+    const feedData = await UserModel.find({
+      $and: [
+        { _id: { $nin: Array.from(hideUserFromFeed) } },
+        { _id: { $ne: loggedInUser._id } },
+      ],
+    })
+    .select("firstName lastName age about gender")
+    .skip(skip)
+    .limit(limit);
+    res.json({ data: feedData });
   } catch (err) {
     res.json({
       message: `${err.message}`,
